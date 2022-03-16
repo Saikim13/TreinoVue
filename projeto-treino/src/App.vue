1f8c3ecf6541978af731e0d7df67674f935f5a83
Saikim <kim_romeu@hotmail.com>
2022-03-16 14:36:25 -0300

    MeuInicio

--- projeto-treino/src/App.vue
+++ projeto-treino/src/App.vue
@@ -2,33 +2,66 @@
   <v-app>
     <v-app-bar
       color="indigo lighten-2"
-      dense
+      app
       dark
     >
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
 
-      <v-toolbar-title>Page title</v-toolbar-title>
+      <v-toolbar-title>Campeonato Brasileiro</v-toolbar-title>
 
     </v-app-bar>
 
+    <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      temporary
+    >
+      <v-list
+        nav
+        dense
+      >
+        <v-list-item-group
+          v-model="group"
+          active-class="indigo lighten-1"
+        >
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Início</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-soccer</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Classificação</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+
     <v-main>
+
+        <MeuInicio/>
 
     </v-main>
   </v-app>
 </template>
 
 <script>
-
+import MeuInicio from './components/MeuInicio.vue'
 
 export default {
   name: 'App',
 
   components: {
-
+    MeuInicio
   },
 
   data: () => ({
-    //
+    drawer: false,
+    group: null,
   }),
 };
 </script>
---
+++ projeto-treino/src/components/MeuInicio.vue
@@ -0,0 +1,30 @@
+<template>
+    <v-container>
+        <h2 class="text-h6 text-center mt-5">Brasileirão Série A</h2>
+
+        <v-img
+            class="inicio-imagem mg-4"
+            src="https://diariodonordeste.verdesmares.com.br/image/contentid/policy:1.2974821:1596806515/Serie-A.png"
+        ></v-img>
+
+        <p class="desc">A Série A do Campeonato Brasileiro de Futebol de 2020, oficialmente Brasileirão Assaí – Série A 2022 por motivos de patrocínio, é a 66.ª edição da principal divisão do futebol brasileiro. A disputa tem o mesmo regulamento dos anos anteriores, quando foi implementado o sistema de pontos corridos.</p>
+    </v-container>
+</template>
+
+<script>
+export default {
+        name: 'MeuInicio'
+}
+</script>
+
+
+<style scoped>
+    .inicio-imagem {
+        border-radius: 3px;
+    }
+
+    .desc {
+        text-align: justify;
+        margin: 4px;
+    }
+</style>
